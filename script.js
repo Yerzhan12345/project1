d669704b88b62e3472aed8b02c8c1ce54cce6ce5
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2022-12-26 21:13:39 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict"; // используется для использования последней версии языка
+
+console.log('arr' + "- object");
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log(incr);
+console.log(decr);
