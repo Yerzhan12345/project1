6cdb392de6f6dc2a9724c4921fa77eceb41e493c
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2022-12-30 12:18:36 +0500

    for loop with loop in loop

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-let num = 51;
-
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
-
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while (num < 55);
-
-for (let i = 1; i <= 10; i++) {
-    if(i == 6){
-        continue; //пропускает данное значение и проходит дальше по условию
-    }
+for (let i = 0; i < 3; i++) {
     console.log(i);
+    for (let j = 0; j < 3; j++) {
+        console.log(j);
+    }
 }
+
+let result = '';
+const lenght = 7;
+
+for(let i = 1; i < lenght; i++) {
+
+    for(let j = 0; j < i; j++){
+        result += "*";
+    }
+    result += '\n';
+}
+
+console.log(result);
