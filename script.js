2bebab6683637785910cd8ad59efb6ea89333465
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2022-12-31 19:22:49 +0500

    Return function

--- script.js
+++ script.js
@@ -1,32 +1,28 @@
-"use strict";
+const usdCurr = 28;
+const eurCurr = 32;
+const discount = 0.9; 
 
-function showFirstMessage(text) {
-    console.log(text);
-    let num = 20;
+function convert (amount, curr) {
+    return curr * amount; 
+}
+// желательно чтобы функция выполняла лишь одно действие. Типа как здесь возращала только значение, а выведение в консоль делалось в другом месте.
+function promotion(result) {
+    console.log(result * discount);
 }
 
-showFirstMessage("Hello World");
+console.log(convert(500, usdCurr));
+convert(500, eurCurr);
 
-// function calc(a, b) {
-//     return (a + b);
-// }
+promotion(convert(500, usdCurr)); // здесь мы можем использовать одну функцию для вызова другой.
 
-// console.log(calc(5,6));
-
-function ret() {
-    let num = 50;
-    return num;
+function test() {
+    for (let i = 0; i < 5; i++){
+        console.log(i);
+        if(i === 3) return // здесь используется данный return чтобы досрочно завершить программу.
+    }
+    console.log("done");
 }
+test();
 
-const anotherNum = ret();
-console.log(anotherNum);
-
-const logger = function () {
-    console.log("Hello");
-};
-
-logger();
-
-const calc = (a, b) => a + b;
-
-calc(2,3);
+function diNothing() {}
+    console.log(diNothing() === undefined);
