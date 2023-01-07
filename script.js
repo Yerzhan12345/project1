76329851067f9302d87e6918523e4373979db666
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2023-01-07 20:38:51 +0500

    exercises with objects and arrays

--- script.js
+++ script.js
@@ -1,99 +1,60 @@
-const personalPlanPeter = {
-    name: "Peter",
-    age: "29",
-    skills: {
-        languages: ['ru', 'eng'],
-        programmingLangs: {
-            js: '20%',
-            php: '10%'
+const shoppingMallData = {
+    shops: [
+        {
+            width: 10,
+            length: 5
         },
-        exp: '1 month'
-    },
-    showAgeAndLangs: function(plan) {
-        const {age} = plan;
-        const{languages} = plan.skills;
-        let str = `Мне ${age} и я владею языками: ${languages}`;
-
-        languages.forEach(function(item, languages) {
-            str = str + `${languages.toUpperCase()}`;
-        });
-        return str;
+        {
+            width: 15,
+            length: 7
+        },
+        {
+            width: 20,
+            length: 5
+        },
+        {
+            width: 8,
+            length: 10
         }
-    } ;
-
-function showExperience(plan) {
-        const {exp} = plan.skills;
-        return exp;
+    ],
+    height: 5,
+    moneyPer1m3: 30,
+    budget: 50000
 }
 
-console.log(showExperience(personalPlanPeter));
-console.log(personalPlanPeter.showAgeAndLangs(personalPlanPeter));
-
-function showProgrammingLangs(plan) {
-    let str = '';
-    const {programmingLangs} = plan.skills;
-    for (let key in programmingLangs) {
-        str += `Язык ${key} изучен на ${programmingLangs[key]}\n`
-    }
-
-    return str;
-}
-
-console.log(showProgrammingLangs(personalPlanPeter));
-
-let user = { name: "John", years: 30 };
-
-let {name, years: age, isAdmin = false} = user;
-
-console.log( name ); // John
-console.log( age ); // 30
-console.log( isAdmin ); // false
-
-let salaries = {
-    "John": 100,
-    "Pete": 300,
-    "Mary": 250
-  };
-
-  function maxSalary (salary) {
-    let
-  }
-
-const family = ['Peter', 'Ann', 'Alex', 'Linda'];
-
-function showFamily(arr) {
-    let message;
-    (arr.length === 0) ? message = 'Семья пуста' : message = 'Семья состоит из: ';
-    
-    arr.forEach(value => {
-        message += `${value} `
+function isBudgetEnough(data) {
+    let square = 0;
+    let area = 0;
+    data.shops.forEach(shops => {
+        square += shops.width * shops.length;
     });
-    return message;
-}
-
-console.log(showFamily(family));
-
-const favoriteCities = ['liSBon', 'ROME', 'miLan', 'Dublin'];
-
-function standardizeStrings(arr) {
-    let message;
-    
-    arr.forEach(value => {
-        console.log(value.toLowerCase())
-    }) ;
-}
-standardizeStrings(favoriteCities);
-
-const someString = 'This is some strange string';
-
-function reverse(str) { // данная функция возвращает принимаемую строку задом наперед
-    let stri = '';
-    let message = str.split('');
-    for (let i = message.length-1; i >= 0; i--) {
-        stri += `${message[i]}`;
-
+    area = square * data.height;
+    if(area * data.moneyPer1m3 >= data.budget) {
+        console.log('Бюджета достаточно');
+    }else {
+        console.log('Бюджета недостаточно');
     }
-    return stri;
+
 }
 
-console.log(reverse(someString));
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    let a = [], b = [], c = [], rest = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+
+console.log(sortStudentsByGroups(students));
