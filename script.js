145598caad3bb1a41226d3a4d9761b4c9b3d48ba
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2023-01-08 11:33:42 +0500

    Changing  data type

--- script.js
+++ script.js
@@ -1,60 +1,40 @@
-const shoppingMallData = {
-    shops: [
-        {
-            width: 10,
-            length: 5
-        },
-        {
-            width: 15,
-            length: 7
-        },
-        {
-            width: 20,
-            length: 5
-        },
-        {
-            width: 8,
-            length: 10
-        }
-    ],
-    height: 5,
-    moneyPer1m3: 30,
-    budget: 50000
+"use strict";
+
+console.log(typeof(String(null)));
+console.log(typeof(String(4))); // Как поменять тип данных
+
+const num = 5;
+
+console.log("https://vk.com/catalog/" + num);
+
+const fontSize = 26 + 'px';
+
+// To Number
+//1)
+console.log(typeof(Number('4')));
+
+console.log(typeof(+'5'));
+
+console.log(typeof(parseInt("15px", 10)));
+
+let answ = +prompt("Hello", "");
+
+//To boolean
+
+// 0, '', null, undefined, NaN; always false
+
+let switcher = null;
+
+if(switcher) {
+    console.log('Working...');
 }
 
-function isBudgetEnough(data) {
-    let square = 0;
-    let area = 0;
-    data.shops.forEach(shops => {
-        square += shops.width * shops.length;
-    });
-    area = square * data.height;
-    if(area * data.moneyPer1m3 >= data.budget) {
-        console.log('Бюджета достаточно');
-    }else {
-        console.log('Бюджета недостаточно');
-    }
+switcher = 1;// мы можем поменять значение null на 1. Означает правду.
 
+if(switcher) {
+    console.log('Working...');
 }
 
+//Как поменять строчную на boolean
 
-const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
-
-function sortStudentsByGroups(arr) {
-    arr.sort();
-    let a = [], b = [], c = [], rest = [];
-    for (let i = 0; i < arr.length; i++) {
-        if (i < 3) {
-            a.push(arr[i]);
-        } else if (i < 6) {
-            b.push(arr[i]);
-        } else if (i < 9) {
-            c.push(arr[i]);
-        } else {
-            rest.push(arr[i]);
-        }
-    }
-    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
-}
-
-console.log(sortStudentsByGroups(students));
+console.log(typeof(!!"4444"));
