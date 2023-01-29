503545521c8c157416af86434d1abfd0e7bd48a1
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2023-01-29 15:43:23 +0500

    Map function

--- js/script.js
+++ js/script.js
@@ -1,15 +1,5 @@
-function morning(name) {
-    return `Good morning ${name.toUpperCase()}`;
-}
-
-function afternoon(name) {
-    return `Good afternoon ${name.toUpperCase()}`;
-}
-
-function greet(name, callback) {
-    const myName = 'John';
-    console.log(`${callback(name)}, my name is ${myName}`);
-}
-
-greet('bobo', morning);
-greet('peter', afternoon);
+const people = [
+    {name: 'bob', age: 20, position: 'developer'},
+    {name: 'marina', age: 23, position: 'manager'},
+    {name: 'alex', age: 30, position: 'CEO'},
+];
