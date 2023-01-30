52d90b5ff7bba3b66abd2ff800d64f6a41fc1600
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2023-01-31 02:50:15 +0500

    date function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-//reduce
-//iterates, callback function
-//reduces to a single value - number, array, object
-//1 parametr ('acc') - total of all calculations
-//2 parametr ('curr') - current itteration/value
- 
-const people = [
-    {name: 'bob', age: 20, position: 'developer', id: 1, salary: 200},
-    {name: 'marina', age: 23, position: 'manager', id: 2, salary: 300},
-    {name: 'alex', age: 30, position: 'CEO', id: 3, salary: 500},
-    {name: 'anna', age: 35, position: 'accountant', id: 4, salary: 500},
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
 ];
 
-const total = people.reduce(function(acc, curentItem){
-    console.log(`total: ${acc}`);
-    console.log(`current money: ${curentItem.salary}`);
-    acc += curentItem.salary;
-    return acc;
-},0)
+const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
-console.log(total);
+const date = new Date();
+
+const month = date.getMonth();
+console.log(months[month]);
+
+const day = date.getDay();
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const sentence = `${days[day]}, ${date.getDate()}, ${
+    months[month]
+}, ${date.getFullYear()}`;
+
+console.log(sentence);
+document.body.innerHTML = sentence;
