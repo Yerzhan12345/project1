bf01505913e6ce5309492678d3c09b064610a2bb
Yerzhan Kulynbetov <yerzhan.kulynbetov@gmail.com>
2023-01-30 16:22:49 +0500

    reduce function

--- js/script.js
+++ js/script.js
@@ -1,17 +1,21 @@
-//find
-//returns signal instance - (in this case object)
-//returns first match, if no match - undefined
-//great for getting unique value
+//reduce
+//iterates, callback function
+//reduces to a single value - number, array, object
+//1 parametr ('acc') - total of all calculations
+//2 parametr ('curr') - current itteration/value
  
 const people = [
-    {name: 'bob', age: 20, position: 'developer', id: 1},
-    {name: 'marina', age: 23, position: 'manager', id: 2},
-    {name: 'alex', age: 30, position: 'CEO', id: 3},
-    {name: 'anna', age: 35, position: 'accountant', id: 4},
+    {name: 'bob', age: 20, position: 'developer', id: 1, salary: 200},
+    {name: 'marina', age: 23, position: 'manager', id: 2, salary: 300},
+    {name: 'alex', age: 30, position: 'CEO', id: 3, salary: 500},
+    {name: 'anna', age: 35, position: 'accountant', id: 4, salary: 500},
 ];
 
-const person = people.find(function(person){
-    return person.id === 3;
-})
+const total = people.reduce(function(acc, curentItem){
+    console.log(`total: ${acc}`);
+    console.log(`current money: ${curentItem.salary}`);
+    acc += curentItem.salary;
+    return acc;
+},0)
 
-console.log(person.name);
+console.log(total);
